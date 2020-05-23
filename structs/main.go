64b2adcf23893efc53a624a09fbd80b5f097f487
Avinash KS <itsavi.ks@gmail.com>
2020-05-24 00:21:57 +0530

    organizing data with struct

---
+++ structs/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+func main() {
+	// p1 := person{"Avinash", "Kunuje"}
+	// p := person{firstName: "Avinash", lastName: "Kunuje"}
+	// fmt.Println(p)
+	// var p person
+	// p.firstName = "Avinash"
+	// p.lastName = "KS"
+	// fmt.Println(p)
+	// fmt.Printf("%+v", p)
+
+	p := person{
+		firstName: "Avinash",
+		lastName:  "Kunuje",
+		contactInfo: contactInfo{
+			email:   "itsavi.ks@gmail.com",
+			zipCode: 560102,
+		},
+	}
+	fmt.Println("ggggg", &p)
+	p.updateName("Kavya")
+	p.print()
+}
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
